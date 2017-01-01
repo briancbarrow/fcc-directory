0479e71b33ffda64aa236b48667d2ba6501b6a41
Brian Barrow <briancbarrow@gmail.com>
2016-12-31 21:24:54 -0700

    tests broke using Jumpsuit, troubleshooting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Render} from 'jumpsuit'
+import state from './states/state'
+
+Render(state, <App />)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  Render(state, <App />)
 });
