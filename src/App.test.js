86d751fa46ebe8bff195069038f6c0c0e1dc56ed
Brian Barrow <briancbarrow@gmail.com>
2017-01-01 19:56:30 -0700

    trying to shallow render in the test to see if that works

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 import {Render} from 'jumpsuit'
 import state from './states/state'
 
-// Render(state, <App />)
-
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  div.setAttribute("id", "app");
+  shallow(<App />);
+  // const div = document.createElement('div');
+  // div.setAttribute("id", "app");
   // ReactDOM.render(<App />, div);
-  Render(state, <App />)
+  // Render(state, <App />)
 });
