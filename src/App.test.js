69393709b20cd86d8949a65d967442d42275f2b8
Brian Barrow <briancbarrow@gmail.com>
2017-01-02 18:58:01 -0700

    trying to get testing to work

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Provider from 'react-redux'
-import { shallow } from 'enzyme'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+// import { shallow } from 'enzyme'
 import App from './App';
 import store from './states/store'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  const div = document.createElement('div')
   ReactDOM.render(
     <Provider store={store}>
       <App />
