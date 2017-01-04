f590d02ef01a7367b0839aeb4f0a1ddeecbe1f7c
Brian Barrow <briancbarrow@gmail.com>
2017-01-03 21:09:31 -0700

    trying to integrate Firebase with redux

--- src/states/store.js
+++ src/states/store.js
@@ -1,5 +1,21 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 import * as reducers from '../reducers/index';
+
+const fbConfig = {}
+const config = {
+  userProfile: 'users',
+  enableLogging: false
+}
+const store = createStore(
+  reducers.directoryReducer, // not sure if I need the ()
+  compose(
+    applyMiddleware([
+      thunk.withExtraArgument(getFirebase)
+    ]),
+    reactReduxFirebase(fbConfig, )
+  )
+)
 
 export default createStore(reducers.directoryReducer, applyMiddleware(thunk));
