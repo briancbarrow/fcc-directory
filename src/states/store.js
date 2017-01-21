990936f1e6e0d3cfed18e53c8bebb18b127b6b01
Brian Barrow <briancbarrow@gmail.com>
2017-01-21 11:57:31 -0700

    added one line of code back to define thunk

--- src/states/store.js
+++ src/states/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
 import * as reducers from '../reducers/index';
 
 export default createStore(reducers.directoryReducer, applyMiddleware(thunk));
